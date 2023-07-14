508902966797120806ca3c770b66cde7c806c7c4
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-15 00:23:54 +0300

    fix pycodestyle file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
-
+"""
+Defines the FileStorage class
+"""
 
 import json
 from models.base_model import BaseModel
 
 
 class FileStorage:
-
+    """
+    Represent an abstracted storage engine
+    """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
+        """Return the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
+        """Add new obj to existing dictionary of instances"""
         if obj:
             k = "{}.{}".format(obj.__class__.__name__, obj.id)
             self.__objects[k] = obj
 
     def save(self):
+        """Save obj dictionaries to json file"""
         dict = {}
         for k, v in self.__objects.items():
             dict[k] = v.to_dict()
-        with open (self.__file_path, 'w') as f:
+        with open(self.__file_path, 'w') as f:
             json.dump(dict, f)
 
     def reload(self):
+        """If json file exists, convert obj dicts back to instances"""
         try:
             with open(self.__file_path, 'r') as f:
                 dict = json.load(f)
@@ -33,8 +41,3 @@ class FileStorage:
                 self.new(eval(v['__class__'])(**v))
         except FileNotFoundError:
             return
-
-
-
-
-
