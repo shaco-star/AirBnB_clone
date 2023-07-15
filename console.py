66b641efbf5a9bcbc8998638a4b7a79c38c74d48
husseinhassan92 <hussein.hassan2020@outlook.com>
2023-07-15 21:52:20 +0300

    update console

--- console.py
+++ console.py
@@ -5,6 +5,12 @@ Entry to command interpreter
 import cmd
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,7 +18,8 @@ class HBNBCommand(cmd.Cmd):
     Defines the HolbertonBnB command interpreter.
     """
     prompt = "(hbnb) "
-    classes = {"BaseModel"}
+    classes = {"BaseModel", "State", "City",
+               "Amenity", "Place", "Review", "User"}
 
     def do_quit(self, line):
         """ Quit command to exit the program"""
