6d02158285fff48645fb28a38ecef3a8920decf0
shaco-star <sherifm846@gmail.com>
2023-07-16 23:07:18 +0300

    Add test

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Test Review for expected behavior"""
+import unittest
+from models.review import Review
+from models.base_model import BaseModel
+
+class TestReview(unittest.TestCase):
+    """Tests to check Review class"""
+
+    def setUp(self):
+        """Test setup"""
+        self.review = Review()
+
+    def test_init(self):
+        """Test init"""
+        self.assertTrue(hasattr(self.review, "place_id"))
+        self.assertTrue(hasattr(self.review, "user_id"))
+        self.assertTrue(hasattr(self.review, "text"))
+
+    def test_inheritance(self):
+        """Test inheritance"""
+        self.assertIsInstance(self.review, BaseModel)
+
+if __name__ == '__main__':
+    """Main class"""
+    unittest.main()
